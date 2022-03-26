551861356c4675f45f56d4f6e57bc88960575047
fungirak <gabilazzarini88@outlook.com>
2022-03-26 02:27:42 -0300

    CRUD Completo, JWT Funcionando.

--- src/main/java/com/ProyectoFinalArgProg/crud/entity/Educacion.java
+++ src/main/java/com/ProyectoFinalArgProg/crud/entity/Educacion.java
@@ -1,13 +1,10 @@
 
 package com.ProyectoFinalArgProg.crud.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 
 /**
